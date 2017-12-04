a3112d6dc84a4ca65e1c65aa84c42fedf13502a2
pavel.polischuk <pavelhan89@ukr.net>
2017-12-04 22:57:43 +0200

    TestCasesData

---
+++ TestSelenium/TestSelenium/LoginTests.cs
@@ -0,0 +1,116 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestSelenium.PageObjects;
+
+
+
+namespace TestSelenium
+{
+    public class LoginTests
+    {
+
+        [SetUp]
+        public void Setup() {
+            
+            PropertyCollection.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        NavigationPanel navPanel = new NavigationPanel(PropertyCollection.driver);
+        HomePage homePage = new HomePage(PropertyCollection.driver);
+        
+        [TestCaseSource("TestCases")]
+        public String Login(String userName, String password)
+        {           
+            LoginPage loginPage = new LoginPage(PropertyCollection.driver);
+            loginPage.goToPage();
+            homePage = loginPage.login(userName, password, loginPage);
+            //wait.Until(ExpectedConditions.ElementToBeSelected(loginPage.multipleLoginForm));
+            Boolean multipleSession = false;
+            //Thread.Sleep(15000);
+            try
+            {
+                PropertyCollection.wait.Until(ExpectedConditions
+                .InvisibilityOfElementLocated(By.CssSelector("#pleaseWaitDiv > div > div > div.modal-header > h4")));
+            
+                if (loginPage.multipleLoginForm.Displayed)
+                {
+                    multipleSession = true;
+                }
+            }catch (NoSuchElementException e)
+            {
+            }catch(WebDriverTimeoutException e)
+            {
+            }
+                    
+            //If multiple dialog appeared, we start new session
+            if ( multipleSession == true)
+            {
+                //Click on new "Create new session button"
+                loginPage.newSessionBtn.Click();
+                Console.WriteLine("Login with creation new Session executed");
+                PropertyCollection.wait.Until(ExpectedConditions.ElementToBeClickable(homePage.copyUrlButton));
+                Assert.AreEqual("Home", PropertyCollection.driver.Title);                
+                Console.WriteLine("Log Out successfull");
+                return navPanel.userNameLabel.Text;
+            } else {                             
+                Console.WriteLine("Login executed");
+                PropertyCollection.wait.Until(ExpectedConditions.ElementToBeClickable(homePage.copyUrlButton));
+                Assert.AreEqual("Home", PropertyCollection.driver.Title);
+                
+                Console.WriteLine("Log Out successfull");
+                return navPanel.userNameLabel.Text;
+            }    
+                                     
+        }
+
+    [TearDown]
+        public void TestTearDown()
+        {
+            homePage.logOut();
+            //PropertyCollection.driver.Close();
+            //PropertyCollection.driver.Quit();
+            //PropertyCollection.driver.Dispose();
+        }
+
+        public static List<TestCaseData> TestCases
+        {
+            get
+            {
+                var testCases = new List<TestCaseData>();
+
+                using (var fs = File.OpenRead(@"D:\SeleniumC#\TestSelenium\TestSelenium\users.csv"))
+                using (var sr = new StreamReader(fs))
+                {
+                    string line = string.Empty;
+                    while (line != null)
+                    {
+                        line = sr.ReadLine();
+                        if (line != null)
+                        {
+                            string[] split = line.Split(new char[] { ';' }, StringSplitOptions.None);
+
+                            String userName = split[0];
+                            String password = split[1];
+                            String expectedUser = split[2];
+                            var testCase = new TestCaseData(userName,password).Returns(expectedUser);
+                            testCases.Add(testCase);
+                        }
+                    }
+                }
+
+                return testCases;
+            }
+        }
+
+    }
+}
+
---
+++ TestSelenium/TestSelenium/TestCasesData.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSelenium
+{
+    public static class TestCasesData
+    {
+        public static List<TestCaseData> TestCases
+        {
+            get
+            {
+                var testCases = new List<TestCaseData>();
+
+                using (var fs = File.OpenRead(@"users.csv"))
+                using (var sr = new StreamReader(fs))
+                {
+                    string line = string.Empty;
+                    while (line != null)
+                    {
+                        line = sr.ReadLine();
+                        if (line != null)
+                        {
+                            string[] split = line.Split(new char[] { ',' },
+                                StringSplitOptions.None);
+
+                            String userName = split[0];
+                            String password= split[1];
+
+                            var testCase = new TestCaseData(userName).Returns(password);
+                            testCases.Add(testCase);
+                        }
+                    }
+                }
+
+                return testCases;
+            }
+        }
+
+    }
+}
